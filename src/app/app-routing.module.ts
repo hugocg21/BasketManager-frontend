0dd3ebd9f17dcdd6fa71a7422bd0406150620e8c
hugocg21 <hugocastanogonzalez@gmail.com>
2024-09-13 20:24:46 +0200

    Updated all app files by adding routes and updating app.component.html and app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayerListComponent } from './components/player-list/player-list.component';
+import { PlayerProfileComponent } from './components/player-profile/player-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/players', pathMatch: 'full' },
+  { path: 'players', component: PlayerListComponent },
+  { path: 'player/:id', component: PlayerProfileComponent },
+  { path: '**', redirectTo: '/calendar' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayerListComponent } from './components/player-list/player-list.component';
+import { MatchStatsComponent } from './components/match-stats/match-stats.component';
+import { PlayerProfileComponent } from './components/player-profile/player-profile.component';
+import { PlayerAttendanceComponent } from './components/player-attendance/player-attendance.component';
+import { PlayerPerformanceComponent } from './components/player-performance/player-performance.component';
+
+import { NgChartsModule } from 'ng2-charts';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerListComponent,
+    MatchStatsComponent,
+    PlayerProfileComponent,
+    PlayerAttendanceComponent,
+    PlayerPerformanceComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgChartsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
